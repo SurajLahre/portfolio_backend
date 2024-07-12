425dd8de8894a71fe1a5a23568232f2318574dff
Suraj Lahre <surajlahre0707@gmail.com>
2024-07-12 19:12:29 +0530

    router setup complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,8 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler";
 
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
-        message: 'ok'
+        message: 'Hellow i am working massage'
     })
 })
 
-export {registerUser}
+export { registerUser }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from 'express';
+
+const app = express();
 
 
 
@@ -10,6 +13,14 @@ dotenv.config(
     })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server Is Running at Port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err)=>{
+        console.log("Mongo Db Connection Failed !!!", err);
+    })
 
 
 
